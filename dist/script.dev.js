c24e456afdf6750820fa606ce47430e701dcc7a8
exxonapol <applevicente1117@gmail.com>
2023-12-20 18:07:58 +0800

    changes

---
+++ dist/script.dev.js
@@ -0,0 +1,16 @@
+// function toggleMenu() {
+//     var menu = document.getElementById("myMenu");
+//     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';}
+//     document.addEventListener('DOMContentLoaded', function () {
+//         const checkbox = document.querySelector('.checkbox');
+//         const menuItems = document.querySelector('.menu-items');
+//         checkbox.addEventListener('change', function () {
+//           if (this.checked) {
+//             menuItems.classList.add('show');
+//           } else {
+//             menuItems.classList.remove('show');
+//           }
+//         });
+//       });
+"use strict";
+//# sourceMappingURL=script.dev.js.map
